2624278675747d7c1c77e8e8a5cc0242b2915d87
Davor Padovan <davor.padovan@gmail.com>
2019-12-23 10:49:02 +0100

    Test

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -9,6 +9,11 @@ export default new Vuetify({
       light: {
         primary: '#30d9a4'
       }
+    },
+    options: {
+      minifyTheme: function (css) {
+        return process.env.NODE_ENV === 'production' ? css.replace(/[\r\n|\r|\n]/g, '') : css
+      }
     }
   }
 })
